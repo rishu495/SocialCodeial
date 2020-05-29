870e8f83ae506e99d0af6ecb4331ec74bb72936e
Rishabh Agrawal <rishabhagrawal495@gmail.com>
2020-05-29 21:18:01 +0530

    Second commit: Added first controller and Router

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home=function(req,res){
+    return res.end("<h4>Controller Working Fine</h4>");
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const app=express();
-const port =8000;
+const port=8000;
+
+app.use("/",require("./routes/index"));
+
 
 app.listen(port,function(err){
     if(err){
