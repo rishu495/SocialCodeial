13a6911962ba42128a1044f0e3c88362b412bc1b
Rishabh Agrawal <rishabhagrawal495@gmail.com>
2020-06-03 14:42:05 +0530

    added static files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express=require("express");
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
 
+app.use(expressLayouts);
 app.use("/",require("./routes/index"));
 app.set("view engine","ejs");
 app.set("views","./views");
+app.use(express.static("assets"));
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 app.listen(port,function(err){
     if(err){
